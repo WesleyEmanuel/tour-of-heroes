7263096792fb12d1ee1a2d6e195efc5283b9000f
Wesley <wesleyemanuel.1517@gmail.com>
2023-03-25 10:57:15 -0300

    criacao do service hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   setHero(hero: Hero): void {
     this.selectedHero = hero;
